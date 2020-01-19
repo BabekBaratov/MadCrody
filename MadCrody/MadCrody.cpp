3c73a3813a50ee601afe0c2812a9ff7e4586e8cc
BabekBaratov <mamashaniko01@gmail.com>
2020-01-19 22:26:29 +0200

    Добавил бота

--- MadCrody/MadCrody.cpp
+++ MadCrody/MadCrody.cpp
@@ -2,23 +2,6 @@
 //
 #include"game.h"
 
-class test
-{
-protected:
-	int x;
-	int y;
-public:
-	void show()
-	{
-		cout << x << endl;
-		cout << y << endl;
-	}
-	int get_x() { return x; }
-	int get_y() { return y; }
-	void set_x(int x) { this->x = x; }
-	void set_y(int y) { this->y = y; }
-};
-
 int main()
 {
 	srand(time(0));
@@ -26,13 +9,6 @@ int main()
 
 	game game;
 	game.start();
-
-
-	/*test ts;
-	ts.set_x(50);
-	ts.set_y(100);
-
-	ts.show();*/
 
 
 	system("pause");
